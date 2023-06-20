3af6ce675f44cae4d092cc235e966d519c94b182
Matheus Soutto <matheussoutto27@gmail.com>
2023-06-19 22:12:17 -0300

    7,4 - array challenge

--- 07_04/script.js
+++ 07_04/script.js
@@ -9,3 +9,28 @@
  */
 
 
+let itemsOnDesk = ['mouse', 'mousepad', 'headset', 'laptop', 'charger', 'flash drive', 'webcam', 'phone'];
+console.log(itemsOnDesk);
+
+// remove last item
+let lastItem = itemsOnDesk.pop();
+console.log(itemsOnDesk);
+
+// add last item as first item on the array;
+itemsOnDesk.unshift(lastItem);
+console.log(itemsOnDesk);
+
+// sort by alphabetical order
+itemsOnDesk.sort();
+console.log(itemsOnDesk);
+
+// find item in the array
+let laptop = itemsOnDesk.find((item) => {
+    if (item == 'laptop') return item;
+});
+console.log(itemsOnDesk);
+
+// remove the item found from the array
+let index = itemsOnDesk.indexOf(laptop);
+itemsOnDesk.splice(index, 1);
+console.log(itemsOnDesk);
